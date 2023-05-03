5bfb6fda54491c148fa7cd9425759b36eb984b94
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2023-05-03 21:33:30 +0530

    Stage01 data_ingestion completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -23,6 +23,7 @@ class DataIngestion:
         logging.info("Entered the data ingestion method or component")
         try:
             df = pd.read_csv("notebook/data/rta_data.csv")
+            df = df.drop(columns=['Time'], axis=1)
             logging.info("Read the dataset as dataframe")
             
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
---
+++ src/components/data_transformation.py
@@ -0,0 +1,33 @@
+
+import os
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler, LabelEncoder
+
+from src.exception import CustomException
+from src.logger import logging
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join("artifacts", "preprocessor.pkl")
+    
+    
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+        
+    
+    def get_data_transformer_object(self):
+        
+        try:
+            pass
+        except:
+            pass
+        
